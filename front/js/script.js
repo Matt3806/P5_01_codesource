8b734eb7600c60415a2a3005fe6a3c9f5bf630db
Matt3806 <matcampus@gmail.com>
2021-12-20 01:00:45 +0100

    création des fichier js et ajout des produits sur l'index

---
+++ front/js/script.js
@@ -0,0 +1,15 @@
+fetch ("http://localhost:3000/api/products")
+.then (data => data.json())
+.then (jsonListKanap => {
+    //console.log(jsonListKanap);
+    for (let jsonKanap of jsonListKanap){
+        document.querySelector(".items").innerHTML += ` <a href="./product.html?id=${jsonKanap._id}">
+                                                            <article>
+                                                            <img src="${jsonKanap.imageUrl}" alt="${jsonKanap.altTxt}">
+                                                            <h3 class="productName">${jsonKanap.name}</h3>
+                                                            <p class="productDescription">${jsonKanap.description}</p>
+                                                            </article>
+                                                        </a> `
+    }
+
+});
